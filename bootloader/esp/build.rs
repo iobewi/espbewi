4458d59a649841258e3c45f743fd3a6d89ae980e
Lionel ORCIL <48251459+ioio2995@users.noreply.github.com>
2026-09-26 17:03:12 +0200

    build: select ESP bootloader linker profile

---
+++ bootloader/esp/build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let linker = if std::env::var_os("CARGO_FEATURE_ESP32C3").is_some() {
+        "espbewi-boot-esp32c3.x"
+    } else if std::env::var_os("CARGO_FEATURE_ESP32S3").is_some() {
+        "espbewi-boot-esp32s3.x"
+    } else {
+        panic!("no supported ESP boot target feature selected");
+    };
+
+    println!("cargo:rustc-link-search={dir}/linker");
+    println!("cargo:rustc-link-arg=-T{linker}");
+    println!("cargo:rerun-if-changed=linker/{linker}");
+}
